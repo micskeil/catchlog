d9468c470fd99147a112204fc81542ac15462805
micskeil <micskeil@gmail.com>
2020-12-20 18:28:59 +0100

    settings page changes

--- src/components/Settings/ProfileSettings.vue
+++ src/components/Settings/ProfileSettings.vue
@@ -8,13 +8,22 @@
           <div class="col-6 font-weight-bold d-flex justify-content-center">
             <img
               class="rounded-circle border"
-              src="../../assets/img/user_1.png"
+              :src="imageUrl"
               width="120"
+              height="120"
             />
           </div>
           <div class="col-6 text-right">
-            <base-button> KÉP CSERÉJE </base-button>
+            <base-button v-on:click="onPickFile()"> KÉP CSERÉJE </base-button>
           </div>
+          <input
+            id="fileInput"
+            type="file"
+            class="d-none"
+            ref="fileInput"
+            accept="image/*"
+            v-on:change="onFilePicked"
+          />
         </div>
         <div class="form-group row form pt-3">
           <label class="col-6 col-form-label font-weight-bold" for="user-name"
@@ -59,6 +68,7 @@
 </template>
 
 <script>
+import firebase from "firebase";
 import BaseButton from "../BaseElements/BaseButton.vue";
 export default {
   components: { BaseButton },
@@ -67,6 +77,10 @@ export default {
       userId: "",
       displayName: "",
       email: "",
+      imageUrl: "",
+      image: null,
+
+      image_src: "",
     };
   },
   watch: {
@@ -84,6 +98,96 @@ export default {
     async logout() {
       await this.$store.dispatch("logout");
       this.$router.push({ name: "Login" });
+    },
+
+    onPickFile() {
+      this.$refs.fileInput.click();
+    },
+
+    onFilePicked(event) {
+      console.log("onFilePicked is running");
+      const files = event.target.files;
+      let filename = files[0].name;
+      console.log("Choosed image: " + filename);
+      if (filename.lastIndexOf(".") <= 0) {
+        return alert("Plase add a valid image file! ");
+      }
+      const fileReader = new FileReader();
+      fileReader.addEventListener("load", () => {
+        this.imageUrl = fileReader.result;
+      });
+      fileReader.readAsDataURL(files[0]);
+      this.image = files[0];
+
+      console.log(this.image);
+    },
+
+    uploadPicture() {
+      let that = this;
+
+      console.log("Uploading " + this.image.name + " to Firebase Storage.");
+
+      // File to upload to Firebase Storage
+      const file = this.image;
+      const metadata = {
+        contentType: "image/*",
+      };
+
+      // Upload file and metadata
+      const storageRef = firebase.storage().ref();
+      console.log(storageRef);
+
+      let regex = new RegExp("[^.]+$");
+      let extension = this.image.name.match(regex);
+
+      const uploadTask = storageRef
+        .child("/" + this.userId + "/avatar" + "." + extension)
+        .put(file, metadata);
+
+      // Listen for state changes, errors, and completion of the upload.
+      uploadTask.on(
+        firebase.storage.TaskEvent.STATE_CHANGED, // or 'state_changed'
+        function(snapshot) {
+          // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
+          var progress =
+            (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+          console.log("Upload is " + progress + "% done");
+          switch (snapshot.state) {
+            case firebase.storage.TaskState.PAUSED: // or 'paused'
+              console.log("Upload is paused");
+              break;
+            case firebase.storage.TaskState.RUNNING: // or 'running'
+              console.log("Upload is running");
+              break;
+          }
+        },
+        function(error) {
+          // A full list of error codes is available at
+          // https://firebase.google.com/docs/storage/web/handle-errors
+          switch (error.code) {
+            case "storage/unauthorized":
+              // User doesn't have permission to access the object
+              break;
+
+            case "storage/canceled":
+              // User canceled the upload
+              break;
+
+            case "storage/unknown":
+              // Unknown error occurred, inspect error.serverResponse
+              break;
+          }
+        },
+        function() {
+          // Upload completed successfully, now we can get the download URL
+          uploadTask.snapshot.ref.getDownloadURL().then(function(downloadURL) {
+            // this.downloadURL = downloadURL;
+            that.image_src = downloadURL;
+
+            console.log("File available at", downloadURL);
+          });
+        }
+      );
     },
   },
   created() {
