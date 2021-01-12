5436bfc516eaea3d1645d65fc48e677031cde6ca
micskeil <micskeil@gmail.com>
2021-01-12 22:37:21 +0100

    add showcomments componenet

---
+++ src/components/Social/ShowComments.vue
@@ -0,0 +1,17 @@
+<template> </template>
+
+<script>
+import { db } from "../../firebase";
+
+export default {
+  props: {
+    postId: {
+      type: String,
+      required: true,
+    },
+  },
+  methods: {
+    loadComments() {},
+  },
+};
+</script>
